8e50dc1f14897e9c84168c32073417dceaa526de
RakUv4 <marek.pilarski@uczen.mechaniktg.pl>
2026-09-04 10:23:20 +0200

    losowane i wypisywanie listy bez powtorzen

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
-        //losowanie tablicy bez powtórzeń
-        int[] wylosowanaTablica = new int[6];
+        //losowanie listy bez powtórzeń
+        ArrayList<Integer> listaWylosowana = new ArrayList<>();
         int liczba;
-        boolean czyZawiera = false;
+       while(listaWylosowana.size()< 6){
+           liczba = rand.nextInt(1,50);
+           if(listaWylosowana.contains(liczba)){
+               continue;
+           }
+           listaWylosowana.add(liczba);
+       }
 
-        for (int i = 0; i < wylosowanaTablica.length; i++) {
-            liczba = rand.nextInt();
-            wylosowanaTablica[i] = rand.nextInt(1, 50);
-            for (int j = 0; j < i; j++) {
-                if(wylosowanaTablica[j] == liczba){
-                    czyZawiera = true;
-                    break;
-                }
-            }
-            if(!czyZawiera){
-                wylosowanaTablica[i]=liczba;
-            }
-        }
+
         //wypisywanie tablicy
-        System.out.println("Tablica losowana: ");
-        for (int elementTablicy:wylosowanaTablica){
-            System.out.println(elementTablicy);
+        System.out.println("Wylosowana lista");
+        System.out.println(listaWylosowana);
         }
 
     }
-}
