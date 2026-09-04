8b00094da9f307dcb11412799ac6acfe8e2ee3cf
RakUv4 <marek.pilarski@uczen.mechaniktg.pl>
2026-09-04 10:05:39 +0200

    losowanie i wypisywanie tablicy

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int[] wylosowanaTablica = new int[6];
 
+        for (int i = 0; i < wylosowanaTablica.length; i++) {
+            wylosowanaTablica[i] = rand.nextInt(1, 50);
+        }
+        //wypisywanie tablicy
+        System.out.println("Tablica losowana: ");
+        for (int elementTablicy:wylosowanaTablica){
+            System.out.println(elementTablicy);
+        }
 
     }
 }
